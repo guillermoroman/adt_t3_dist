3e5deeb70b415af5e88cf3519db609dc2925e6ae
guillermoroman <guillermoroman@outlook.com>
2024-01-09 12:34:45 +0100

    "Finish"

--- src/JDBC_DDL/JDBC_DDL.java
+++ src/JDBC_DDL/JDBC_DDL.java
@@ -1,2 +1,58 @@
-package JDBC_DDL;public class JDBC_DDL {
+package JDBC_DDL;
+
+import databaseConnection.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC_DDL {
+    public static void main(String[] args) {
+        //createTable();
+        //alterTable();
+        dropTable();
+    }
+
+    public static void createTable() {
+        String sql = "CREATE TABLE clientes1 (" +
+                "dni CHAR(9) NOT NULL," +
+                "apellido CHAR(20) NOT NULL," +
+                "cp INTEGER," +
+                "PRIMARY KEY (dni));";
+
+        try (Connection c = DatabaseConnection.getConnection();
+             Statement stmt = c.createStatement()){
+            stmt.execute(sql);
+            System.out.println("Tabla 'clientes1' creada con éxito");
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void alterTable(){
+        String sql = "ALTER TABLE clientes1 MODIFY cp CHAR(5)";
+
+        try (Connection c = DatabaseConnection.getConnection();
+             Statement stmt = c.createStatement()){
+            stmt.execute(sql);
+            System.out.println("La columna 'cp' de la tabla 'clientes1' ha sido modificada");
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void dropTable() {
+        String sql = "DROP TABLE IF EXISTS clientes1";
+
+        try (Connection c = DatabaseConnection.getConnection();
+             Statement stmt = c.createStatement()){
+            stmt.execute(sql);
+            System.out.println("La tabla 'clientes1' se ha eliminado");
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/JDBC_transacciones/JDBC_transacciones.java
+++ src/JDBC_transacciones/JDBC_transacciones.java
@@ -1,2 +1,51 @@
-package JDBC_transacciones;public class JDBC_transacciones {
+package JDBC_transacciones;
+
+import databaseConnection.DatabaseConnection;
+import org.mariadb.jdbc.export.Prepare;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBC_transacciones {
+    public static void main(String[] args) {
+        try (Connection c = DatabaseConnection.getConnection()){
+            try (PreparedStatement sInsert = c.prepareStatement("INSERT INTO " +
+                    "clientes1 (dni, apellido, cp) VALUES (?, ?, ?);")){
+
+                c.setAutoCommit(false);
+
+                sInsert.setString(1, "98237469V");
+                sInsert.setString(2, "PEREZ");
+                sInsert.setString(3, "45623");
+                sInsert.executeUpdate();
+
+                sInsert.setString(1, "92836477S");
+                sInsert.setString(2, "MARTINEZ");
+                sInsert.setString(3, "56987");
+                sInsert.executeUpdate();
+
+                sInsert.setString(1, "90123445A");
+                sInsert.setString(2, "MOLINA");
+                sInsert.setString(3, "35153");
+                sInsert.executeUpdate();
+
+                c.commit();
+
+            } catch (SQLException e){
+                try {
+                    c.rollback();
+                    System.err.println("Se hace ROLLBACK");
+                } catch (SQLException er) {
+                    System.err.println("Error haciendo ROLLBACK");
+
+                }
+            }
+
+        } catch (SQLException e){
+            System.err.println("Error de conexión");
+
+            e.printStackTrace();
+        }
+    }
 }
